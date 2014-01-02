9c4cd6ff1a565c3e294b0a72f68bd7d3c312715b
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-02 15:12:12 +0000

    Add trivial though meaningful comment.

--- sort/isort_test.go
+++ sort/isort_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestInsertionSortSortsReversedInts(t *testing.T) {
 	correctlySortsReversedInts(t, 1e4, func(ns []int) []int {
+		// In-place sort.
 		InsertionSort(ns)
 		return ns
 	})
