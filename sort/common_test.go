301da87b6d9f0ac64adeb8de33ddb85ab03f62f7
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-02 15:09:22 +0000

    Share common testing code.
    Reduces duplication.

---
+++ sort/common_test.go
@@ -0,0 +1,21 @@
+package algoholic
+
+import "testing"
+
+// A simple test which sorts a reversed set of integers and ensures they are correctly sorted
+// afterward.
+func correctlySortsReversedInts(t *testing.T, max int, doSort func([]int) []int) {
+	ns := make([]int, max)
+
+	for i := 0; i < max; i++ {
+		ns[i] = max - i
+	}
+
+	ns = doSort(ns)
+
+	for i := 0; i < max; i++ {
+		if ns[i] != i+1 {
+			t.Errorf("Index %d is not sorted = %d, expected %d.", i, ns[i], i+1)
+		}
+	}
+}
--- sort/msort_test.go
+++ sort/msort_test.go
@@ -2,22 +2,8 @@ package algoholic
 
 import "testing"
 
-const N = 1e6
-
 // A simple test which sorts a reversed set of integers and ensures they are correctly sorted
 // afterward.
-func TestSorts(t *testing.T) {
-	ns := make([]int, N)
-
-	for i := 0; i < N; i++ {
-		ns[i] = N - i
-	}
-
-	ns = MergeSort(ns)
-
-	for i := 0; i < N; i++ {
-		if ns[i] != i+1 {
-			t.Errorf("Index %d is not sorted = %d, expected %d.", i, ns[i], i+1)
-		}
-	}
+func TestMergeSortSortsReversedInts(t *testing.T) {
+	correctlySortsReversedInts(t, 1e6, MergeSort)
 }
