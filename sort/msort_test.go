56f07c977fca5cc56a83959c46e69cf6a6a98749
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-02 15:56:46 +0000

    Test Merge behaves correctly with empty slice inputs.

--- sort/msort_test.go
+++ sort/msort_test.go
@@ -7,3 +7,30 @@ import "testing"
 func TestMergeSortSortsReversedInts(t *testing.T) {
 	correctlySortsReversedInts(t, 1e6, MergeSort)
 }
+
+func TestMergeMergesEmptySlicesCorrectly(t *testing.T) {
+	const N = 10
+
+	empty := []int{}
+	ns := make([]int, N)
+
+	for i := 0; i < N; i++ {
+		ns[i] = i + 1
+	}
+
+	if zero := len(Merge(empty, empty)); zero != 0 {
+		t.Errorf("Merge([]int{}, []int{}) has %d length, not expected 0.", zero)
+	}
+
+	merged := Merge(empty, ns)
+
+	if !slicesAreEqual(ns, merged) {
+		t.Error("Merge([]int{}, ns) does not return ns.")
+	}
+
+	merged = Merge(ns, empty)
+
+	if !slicesAreEqual(ns, merged) {
+		t.Error("Merge(ns, []int{}) does not return ns.")
+	}
+}
