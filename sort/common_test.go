3f5c97b6e7d9fa12d5c356a99a50d0a4f8e797ca
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-03 00:22:20 +0000

    Add shared benchmarking code.

--- sort/common_test.go
+++ sort/common_test.go
@@ -2,14 +2,16 @@ package algoholic
 
 import "testing"
 
+func benchmarkSortingReversedInts(b *testing.B, doSort func([]int) []int) {
+	ns := createReversedInts(b.N)
+	b.ResetTimer()
+	doSort(ns)
+}
+
 // A simple test which sorts a reversed set of integers and ensures they are correctly sorted
 // afterward.
 func correctlySortsReversedInts(t *testing.T, max int, doSort func([]int) []int) {
-	ns := make([]int, max)
-
-	for i := 0; i < max; i++ {
-		ns[i] = max - i
-	}
+	ns := createReversedInts(max)
 
 	sorted := doSort(ns)
 
@@ -18,6 +20,16 @@ func correctlySortsReversedInts(t *testing.T, max int, doSort func([]int) []int)
 			t.Fatalf("Unsorted at index %d: got %d, expected %d.", i, sorted[i], i+1)
 		}
 	}
+}
+
+func createReversedInts(max int) []int {
+	ret := make([]int, max)
+
+	for i := 0; i < max; i++ {
+		ret[i] = max - i
+	}
+
+	return ret
 }
 
 func slicesAreEqual(ns1, ns2 []int) bool {
