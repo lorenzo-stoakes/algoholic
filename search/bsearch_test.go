71ecef64dfe4cce6557d4918258e516e15285ed4
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-03 23:28:21 +0000

    Separate out searching test code.

--- search/bsearch_test.go
+++ search/bsearch_test.go
@@ -3,37 +3,13 @@ package algoholic
 import "testing"
 
 func TestEmptyBinarySearch(t *testing.T) {
-	if res := BinarySearch(nil, 123); res != -1 {
-		t.Errorf("Binary search of nil resulted in %d, not expected -1.", res)
-	}
-
-	if res := BinarySearch([]int{}, 123); res != -1 {
-		t.Errorf("Binary search of []int{} resulted in %d, not expected -1.", res)
-	}
+	correctlySearchesEmptySlice(t, BinarySearch)
 }
 
 func TestBinarySearchCantFindMissingItem(t *testing.T) {
-	for length := 0; length <= 100; length++ {
-		ns := generateRange(1, length)
-		if res := BinarySearch(ns, 0); res != -1 {
-			t.Fatalf("Binary search of slice of [1, %d] for 0 returned %d, not expected -1.",
-				length, res)
-		}
-		if res := BinarySearch(ns, length+1); res != -1 {
-			t.Fatalf("Binary search of slice of [1, %d] for %d returned %d, not expected -1.",
-				length, length+1, res)
-		}
-	}
+	correctlyFailsToFindMissingItem(t, BinarySearch)
 }
 
 func TestBinarySearchFindsItem(t *testing.T) {
-	for length := 1; length <= 1e3; length++ {
-		ns := generateRange(1, length)
-		for i := 1; i <= length; i++ {
-			if res := BinarySearch(ns, i); res != i-1 {
-				t.Fatalf("Binary search did not find %d at index %d of [1, %d], returned %d.",
-					i, i-1, length, res)
-			}
-		}
-	}
+	correctlyFindsItem(t, BinarySearch)
 }
--- search/common_test.go
+++ search/common_test.go
@@ -1,5 +1,8 @@
 package algoholic
 
+import "testing"
+
+type searchFunc func([]int, int) int
 func generateRange(from, to int) []int {
 	length := to - from + 1
 
@@ -14,4 +17,40 @@ func generateRange(from, to int) []int {
 	}
 
 	return ret
+}
+
+func correctlySearchesEmptySlice(t *testing.T, doSearch searchFunc) {
+	if res := doSearch(nil, 123); res != -1 {
+		t.Errorf("Search of nil returned %d, not expected -1.", res)
+	}
+
+	if res := doSearch([]int{}, 123); res != -1 {
+		t.Errorf("Search of []int{} returned %d, not expected -1.", res)
+	}
+}
+
+func correctlyFailsToFindMissingItem(t *testing.T, doSearch searchFunc) {
+	for length := 0; length <= 100; length++ {
+		ns := generateRange(1, length)
+		if res := doSearch(ns, 0); res != -1 {
+			t.Fatalf("Search of slice of [1, %d] for 0 returned %d, not expected -1.",
+				length, res)
+		}
+		if res := doSearch(ns, length+1); res != -1 {
+			t.Fatalf("Search of slice of [1, %d] for %d returned %d, not expected -1.",
+				length, length+1, res)
+		}
+	}
+}
+
+func correctlyFindsItem(t *testing.T, doSearch searchFunc) {
+	for length := 1; length <= 1e3; length++ {
+		ns := generateRange(1, length)
+		for i := 1; i <= length; i++ {
+			if res := doSearch(ns, i); res != i-1 {
+				t.Fatalf("Search of slice of [1, %d] for %d returned %d, not expected %d.",
+					length, i, res, i-1)
+			}
+		}
+	}
 }
