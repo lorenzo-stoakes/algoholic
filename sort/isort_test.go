12a03615670c834f15f28c136f26cb75ba168f65
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-03 00:22:48 +0000

    Separate out isort returning a slice.

--- sort/isort_test.go
+++ sort/isort_test.go
@@ -2,10 +2,14 @@ package algoholic
 
 import "testing"
 
+// For convenience implement variation on isort which returns the slice once sorted.
+func isort2(ns []int) []int {
+	// In-place.
+	InsertionSort(ns)
+	return ns
+}
+
 func TestInsertionSortSortsReversedInts(t *testing.T) {
-	correctlySortsReversedInts(t, 1e4, func(ns []int) []int {
-		// In-place sort.
-		InsertionSort(ns)
-		return ns
-	})
+	correctlySortsReversedInts(t, 1e4, isort2)
+}
 }
