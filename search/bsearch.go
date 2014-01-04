f20ce1402a19548a94dbcadcc864713916fe0cfa
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-04 00:41:40 +0000

    Nest recursive function in outer function.
    Neater, better encapsulation.

--- search/bsearch.go
+++ search/bsearch.go
@@ -1,25 +1,26 @@
 package algoholic
 
-func binarySearchImpl(ns []int, n, offset int) int {
-	if len(ns) == 0 {
-		return -1
-	}
-
-	half := len(ns) / 2
-	val := ns[half]
-
-	switch {
-	case n == val:
-		return half + offset
-	case n < val:
-		return binarySearchImpl(ns[:half], n, offset)
-	default:
-		// n > val
-		return binarySearchImpl(ns[half+1:], n, offset+half+1)
-	}
-}
-
 func BinarySearchRecursive(ns []int, n int) int {
+	var binarySearchImpl func([]int, int, int) int
+	binarySearchImpl = func(ns []int, n, offset int) int {
+		if len(ns) == 0 {
+			return -1
+		}
+
+		half := len(ns) / 2
+		val := ns[half]
+
+		switch {
+		case n == val:
+			return half + offset
+		case n < val:
+			return binarySearchImpl(ns[:half], n, offset)
+		default:
+			// n > val
+			return binarySearchImpl(ns[half+1:], n, offset+half+1)
+		}
+	}
+
 	return binarySearchImpl(ns, n, 0)
 }
 
