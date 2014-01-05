ef9b86dd9dd00b1ff3de304f3260e868b0a888e9
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-05 14:46:44 +0000

    Add commentary to linear search.

--- search/lsearch.go
+++ search/lsearch.go
@@ -1,11 +1,18 @@
 package algoholic
 
+// Linear Search - Given an input slice and a sought value, return the index of that value in
+// the slice or -1 if it can't be found.
+
+// Linear Search, O(n) worst-case.
 func LinearSearch(ns []int, n int) int {
+	// Simply iterate through every item in the slice, short-circuiting and returning should we
+	// find the saught value.
 	for i, m := range ns {
 		if m == n {
 			return i
 		}
 	}
 
+	// We haven't found the value.
 	return -1
 }
