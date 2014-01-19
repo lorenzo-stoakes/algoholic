5df54c7c72cd294611837f651f2fcfdc0c554929
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-19 21:39:47 +0000

    We can do better with Fisher-Yates :)

--- src/combinatorial/shuffle/shuffle_test.go
+++ src/combinatorial/shuffle/shuffle_test.go
@@ -6,9 +6,9 @@ const BENCHMARK_LENGTH = 10
 
 func TestFisherYatesShufflesEvenly(t *testing.T) {
 	// Set an arbitrary maximum error from expectation - We expect each number to occur within
-	// 1% of the probability in a list of length 10 after 1e6 iterations.
+	// 0.7% of the probability in a list of length 10 after 1e6 iterations.
 	// TODO: Determine maximum error mathematically.
-	checkShufflesEvenly(t, ShuffleFisherYates, 10, 1e6, 0.01)
+	checkShufflesEvenly(t, ShuffleFisherYates, 10, 1e6, 0.007)
 }
 
 func TestRandomOrderShuffleShufflesEvenly(t *testing.T) {
