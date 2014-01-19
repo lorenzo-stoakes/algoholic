765834d4380829547004dd347382dfd378b3bd04
Lorenzo Stoakes <lstoakes@gmail.com>
2014-01-19 22:54:40 +0000

    Update tests to reflect better seeding.
    We have to lower our expectations a bit.

--- src/combinatorial/shuffle/shuffle_test.go
+++ src/combinatorial/shuffle/shuffle_test.go
@@ -6,16 +6,16 @@ const BENCHMARK_LENGTH = 10
 
 func TestFisherYatesShufflesEvenly(t *testing.T) {
 	// Set an arbitrary maximum error from expectation - We expect each number to occur within
-	// 0.7% of the probability in a list of length 10 after 1e6 iterations.
+	// 1% of the probability in a list of length 10 after 1e6 iterations.
 	// TODO: Determine maximum error mathematically.
-	checkShufflesEvenly(t, ShuffleFisherYates, 10, 1e6, 0.007)
+	checkShufflesEvenly(t, ShuffleFisherYates, 10, 1e6, 0.01)
 }
 
 func TestRandomOrderShuffleShufflesEvenly(t *testing.T) {
 	// Set an arbitrary maximum error from expectation - We expect each number to occur within
-	// 1% of the probability in a list of length 10 after 1e6 iterations.
+	// 1.1% of the probability in a list of length 10 after 1e6 iterations.
 	// TODO: Determine maximum error mathematically.
-	checkShufflesEvenly(t, ShuffleRandomSort, 10, 1e6, 0.01)
+	checkShufflesEvenly(t, ShuffleRandomSort, 10, 1e6, 0.011)
 }
 
 func BenchmarkFisherYatesShuffle(b *testing.B) {
