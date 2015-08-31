464213fd60550e0f739c4dcf785916ac7b60bb5d
Lorenzo Stoakes <lstoakes@gmail.com>
2015-08-31 15:09:44 +0100

    array_type enum is part of the test.

--- sort/sort.h
+++ sort/sort.h
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 
-enum array_type { ARR_REVERSE_SORTED, ARR_SORTED, ARR_RANDOM };
 typedef int *(*sort_fn_t)(int[], const ssize_t);
 
 int *algoholic_isort(int ns[], const ssize_t len);
--- sort/test.h
+++ sort/test.h
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include "sort.h"
 
+enum array_type { ARR_REVERSE_SORTED, ARR_SORTED, ARR_RANDOM };
+
 bool test_sort(const sort_fn_t sort, const size_t len,
 	const enum array_type type);
 
